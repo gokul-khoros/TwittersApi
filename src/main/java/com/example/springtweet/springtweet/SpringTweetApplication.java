7a1a07dd0955082265598326ddee5904aefc2339
gokul-khoros <gokulreko@gmail.com>
2020-11-18 17:07:32 +0530

    gethometimeline, updateStatus features are added

--- src/main/java/com/example/springtweet/springtweet/SpringTweetApplication.java
+++ src/main/java/com/example/springtweet/springtweet/SpringTweetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringtweetApplication {
+public class SpringTweetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringtweetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTweetApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/springtweet/springtweet/controller/TwitterControlller.java
@@ -0,0 +1,32 @@
+package com.example.springtweet.springtweet.controller;
+
+import org.springframework.web.bind.annotation.*;
+import twitter4j.*;
+
+@RestController
+@RequestMapping(value = "/api/1.0/twitter")
+public class TwitterControlller {
+
+    Twitter twitter = TwitterFactory.getSingleton();
+
+    @GetMapping(value = "/timeline")
+    public ResponseList<Status> getTweets() {
+        try {
+            return twitter.getHomeTimeline();
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @PostMapping(value = "/tweet")
+    private String getTimeLine(@RequestBody String tweetMessage) {
+        try {
+            twitter.updateStatus(tweetMessage);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        String statement = "Statement Successfully posted : " + tweetMessage;
+        return statement;
+    }
+}
