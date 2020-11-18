a8bd03dc7abcdfa1a6f89fed5b87d30ddc4792aa
gokul-khoros <gokulreko@gmail.com>
2020-11-18 15:52:29 +0530

    new branch for spring

---
+++ src/main/java/com/example/springtweet/springtweet/SpringtweetApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtweet.springtweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtweet/springtweet/SpringtweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtweet.springtweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
