d551e3d529d2d068e805a88ce65a680175707b61
gokul-khoros <gokulreko@gmail.com>
2020-11-20 14:54:23 +0530

    importing only required lib files

--- src/main/java/com/example/springtweet/springtweet/controller/TwitterController.java
+++ src/main/java/com/example/springtweet/springtweet/controller/TwitterController.java
@@ -3,11 +3,20 @@ package com.example.springtweet.springtweet.controller;
 import com.example.springtweet.springtweet.exceptionHandler.CustomException;
 import com.example.springtweet.springtweet.exceptionHandler.SuccessMessage;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.multipart.MultipartFile;
-import twitter4j.*;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.StatusUpdate;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "/api/1.0/twitter")
@@ -16,12 +25,12 @@ public class TwitterController {
     Twitter twitter = TwitterFactory.getSingleton();
 
     @GetMapping(value = "/timeline")
-    public ResponseList<Status> getTimeLine() {
+    public Object getTimeLine() {
         try {
             return twitter.getHomeTimeline();
         } catch (TwitterException e) {
             e.getStackTrace();
-            return (ResponseList<Status>) e;
+            return e;
         }
     }
 
