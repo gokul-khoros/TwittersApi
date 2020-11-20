5404939fe5e14a98a2689eac0857ba3b6d1e4ead
gokul-khoros <70262858+gokul-khoros@users.noreply.github.com>
2020-11-20 15:18:21 +0530

    Delete TwitterControlller.java

--- src/main/java/com/example/springtweet/springtweet/controller/TwitterControlller.java
+++
@@ -1,9 +0,0 @@
-package com.example.springtweet.springtweet.controller;
-
-import org.springframework.web.bind.annotation.*;
-import twitter4j.*;
-
-@RestController
-public class TwitterControlller {
-
-}
