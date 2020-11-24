f731f8064be560e47bc25ab49be780731ca397a8
gokul-khoros <gokulreko@gmail.com>
2020-11-24 17:03:55 +0530

    unit testing for postTweet

--- src/test/java/com/example/springtweet/springtweet/controller/TwitterControllerTest.java
+++ src/test/java/com/example/springtweet/springtweet/controller/TwitterControllerTest.java
@@ -4,45 +4,40 @@ import com.example.springtweet.springtweet.exceptionHandler.CustomException;
 import com.example.springtweet.springtweet.exceptionHandler.SuccessMessage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.multipart.MultipartFile;
-import twitter4j.TwitterException;
-
-import java.util.Collections;
-import java.util.List;
+import twitter4j.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
- public class TwitterControllerTest {
+public class TwitterControllerTest {
 
     @Mock
-     TwitterController twitterController;
+    Twitter twitter;
+
+    @InjectMocks
+    TwitterController twitterController;
 
     @Test
     public void getTimeLineTest() throws TwitterException {
-        when(twitterController.getTimeLine()).thenReturn(Collections.singletonList("Tweet from Timeline"));
-        List<String> getTimeLineMsg = twitterController.getTimeLine();
-        assertEquals(Collections.singletonList("Tweet from Timeline"),getTimeLineMsg);
+        Status response = (Status) twitter.getHomeTimeline();
+        when(twitter.getHomeTimeline()).thenReturn((ResponseList<Status>) response);
+        ResponseList<Status> getTimeLineMsg = twitterController.getTimeLine();
+        assertEquals(response, getTimeLineMsg);
     }
 
     @Test
-    public void postTweetTest() throws CustomException, SuccessMessage {
-        MultipartFile file = null;
-        String tweet = "yoyo";
-        Mockito.when(twitterController.postTweet(any(),any())).thenReturn(tweet);
-        String json = twitterController.postTweet(file,tweet);
-        String expect = "yoyo";
-        assertEquals(expect,json);
+    public void postTweetTest() throws CustomException, SuccessMessage, TwitterException {
+        Status status = null;
+        when(twitter.updateStatus(anyString())).thenReturn(status);
+        Status twitterStatus = twitterController.postTweet("hello");
+        assertEquals(twitterStatus, status);
     }
-
 
 
 }
